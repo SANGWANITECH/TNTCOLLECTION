dc3590268a4f5beed456143094e6e9d15284f13c
wisdomphiri408 <wisdomphiri408@gmail.com>
2025-11-19 21:24:55 +0200

    Implement session management and user redirection in

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -45,6 +45,13 @@ export async function updateSession(request: NextRequest) {
     return NextResponse.redirect(url)
   }
 
+  //if user is signed in and tries to access /auth ---> redirect to admin
+  if(user && request.nextUrl.pathname.startsWith('/auth')){
+      const url = request.nextUrl.clone();
+      url.pathname = '/admin';
+      return NextResponse.redirect(url);
+  }
+
   // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
   // creating a new response object with NextResponse.next() make sure to:
   // 1. Pass the request in it, like so:
