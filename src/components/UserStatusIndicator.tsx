28b0288ac1ade5f4bcd2d61c63abd485a81f0f97
wisdomphiri408 <wisdomphiri408@gmail.com>
2025-11-19 21:43:26 +0200

    Refactor UserStatusIndicator to remove session management and simplify login state handling

--- src/components/UserStatusIndicator.tsx
+++ src/components/UserStatusIndicator.tsx
@@ -3,33 +3,19 @@
 import { User } from "lucide-react";
 import { NextPage } from "next";
 import Link from "next/link";
-import {createClient} from "@/utils/supabase/client";
-import {useEffect, useState} from "react";
+
 
 const UserStatusIndicator:NextPage = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const supabase = createClient();
-
-    useEffect(() => {
-        supabase.auth.getSession().then(({data: {session} }) => {
-            setIsLoggedIn(!!session)
-        });
-
-        const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
-            setIsLoggedIn(!!session)
-        })
-
-        return () => listener.subscription.unsubscribe()
-    }, [supabase]);
+    const isLoggedIn = false
 
 
     return(
         <div className="relative">
             {isLoggedIn ?(
-            <Link href={'/admin'}>
+            <div>
                 <User className="w-4 h-4"/>
                 <div className="absolute h-2 w-2 rounded-full bg-green-500 top-[-4px] right-[-4px]"/>
-            </Link>):(
+            </div>):(
                 <Link href={'/auth/login'}>
                     <User className="w-4 h-4"/>
                 </Link>
