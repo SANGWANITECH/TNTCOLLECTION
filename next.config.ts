41198664e6716ad277dc96549ea7b9c77da5bd8d
wisdomphiri408 <wisdomphiri408@gmail.com>
2025-12-09 21:37:28 +0200

    Add additional image domain to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['ovapnnyjvmiqnoqgdarc.supabase.co'],
+    domains: ['images.pexels.com','ovapnnyjvmiqnoqgdarc.supabase.co'],
   },
 };
 
