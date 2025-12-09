551a4e9fa12441eb74d97d2c3c00a17cfaf0b6fc
wisdomphiri408 <wisdomphiri408@gmail.com>
2025-12-09 21:25:44 +0200

    Remove unused image domains from Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-<<<<<<< HEAD
-    domains: ['fakestoreapi.com','images.pexels.com','ovapnnyjvmiqnoqgdarc.supabase.co'],
-=======
-    domains: ['fakestoreapi.com','images.pexels.com','images.unsplash.com','ovapnnyjvmiqnoqgdarc.supabase.co'],
->>>>>>> f447cb222f019cb166adfe00a6ca6368bd7f217e
+    domains: ['ovapnnyjvmiqnoqgdarc.supabase.co'],
   },
 };
 
