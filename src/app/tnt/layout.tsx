c22db9df4a71af53984c507ef2c895ad739ef872
wisdomphiri408 <wisdomphiri408@gmail.com>
2025-12-09 21:41:12 +0200

    Refactor layout.tsx to properly wrap components with CartProvider

--- src/app/tnt/layout.tsx
+++ src/app/tnt/layout.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import Header from "@/components/Heade";
 import Footer from "@/components/Footer";
-import { CartProvider } from "@/context/CartProvider";
+
 
 export default function AdminLayout({
                                         children,
@@ -13,13 +13,11 @@ export default function AdminLayout({
         <div >
             <div className={'antialiased min-h-screen flex flex-col'}>
                 {/* Main content */}
-                <CartProvider>
                     <Header />
                     <main className="pt-21 pb-20 flex-grow">
                         {children}
                     </main>
                     <Footer />
-                </CartProvider>
             </div>
         </div>
     );
