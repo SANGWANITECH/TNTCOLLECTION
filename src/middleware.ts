de14dd320455367056b32ede7347c720b5d769cf
wisdomphiri408 <wisdomphiri408@gmail.com>
2025-12-22 17:48:59 +0200

    Add context providers for product addition and filtering in admin layout

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,5 +6,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/','/admin/:path*','/tnt/auth/:path*', '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',],
+  matcher: ['/','/admin/:path*','/tnt/auth/:path*'],
 }
