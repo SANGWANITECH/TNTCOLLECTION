d37cfc9ed6ddb9b9994da8412b3a363a5cd56003
wisdomphiri408 <wisdomphiri408@gmail.com>
2025-12-23 09:32:57 +0200

    added metadata to pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
+import { Metadata } from "next";
 import FeaturedContent from "@/components/home/FeaturedContent";
 import Hero from "@/components/home/Hero";
+
+export const metadata: Metadata = {
+    description: "TNT COLLECTION: Your destination for curated modern essentials. Shop our exclusive range of high-quality products designed for clean, simple living.",
+
+    openGraph: {
+        title: "TNT COLLECTION | Modern Curated Essentials",
+        description: "Discover simple, stylish products that elevate your everyday life.",
+        url: "https://tnt-collection.store",
+        images: [
+            {
+                url: "/images/home banner.jpg",
+                width: 1200,
+                height: 630,
+                alt: "TNT COLLECTION Home Page Preview",
+            },
+        ],
+    },
+};
 
 
 export default function Home() {
