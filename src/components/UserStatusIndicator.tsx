694f030d8a5a679f26d75c0caeaab08449a2e7fa
wisdom phiri <wisdomphiri408@gmail.com>
2026-06-02 12:34:15 +0200

    refactored authentication page

--- src/components/UserStatusIndicator.tsx
+++ src/components/UserStatusIndicator.tsx
@@ -4,25 +4,14 @@ import { User } from "lucide-react";
 import { NextPage } from "next";
 import Link from "next/link";
 
-
-const UserStatusIndicator:NextPage = () => {
-    const isLoggedIn = false
-
-
-    return(
-        <div className="relative">
-            {isLoggedIn ?(
-            <div>
-                <User className="w-4 h-4"/>
-                <div className="absolute h-2 w-2 rounded-full bg-green-500 top-[-4px] right-[-4px]"/>
-            </div>):(
-                <Link href={'/admin'}>
-                    <User className="w-4 h-4"/>
-                </Link>
-            )}
-
-        </div>
-    )
-}
+const UserStatusIndicator: NextPage = () => {
+  return (
+    <div className="relative">
+      <Link href="/admin">
+        <User className="w-4 h-4" />
+      </Link>
+    </div>
+  );
+};
 
 export default UserStatusIndicator;
